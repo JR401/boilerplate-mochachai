94c3ddc8a7ae91b05ae156d7aa3d0b7e55af1a4b
JR401 <joramiranda8@gmail.com>
2024-11-21 11:00:02 -0600

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -20,14 +20,16 @@ suite('Unit Tests', function () {
       assert.fail(isOk("I'm truthy"), 'A string is truthy');
       assert.fail(isOk(true), 'true is truthy');
     });
-    // #4
-    test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
-    });
+    // #4 
+    test('#isTrue, #isNotTrue', function() {
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'double negation of a truthy is true');
+      assert.isNotTrue(
+      { value: 'truthy' },
+      'A truthy object is NOT TRUE (neither is false...)'
+    );
   });
-
+})
   // -----------------------------------------------------------------------------
 
   suite('Equality', function () {
