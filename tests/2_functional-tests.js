2bc5a9acbf1336f49f8e4d279d1c4bca7a4102e8
JR401 <joramiranda8@gmail.com>
2024-11-27 08:21:44 -0600

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -91,10 +91,16 @@ suite('Functional Tests with Zombie.js', function () {
       });
     });
     // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+    test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
+      // fill the form, and submit.
+      browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        
+        done();
+      });
     });
   });
 });
